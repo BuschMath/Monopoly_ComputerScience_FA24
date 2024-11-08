cfa9bcfe706a4fb78304491979a910883da7cf4c
BladedKey51 <lanepopp123@gmail.com>
2024-11-08 09:29:36 -0600

    Base Files

---
+++ Monoply Project/Monopoly Main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include "HowManyPlayers.h"
+
+int main ()
+{
+
+
+
+/*	int playerselect;
+	std::cout << "please input up to 8 number repersenting the character you would like to select" << std::endl;
+
+	std::cin >> playerselect;
+
+	while (9 < playerselect < 0)
+	{
+		if (playerselect == 1) {
+			std::cin >> playerselect;
+		}
+		if (playerselect == 2) {
+			std::cin >> playerselect;
+		}
+		if (playerselect == 3) {
+			std::cin >> playerselect;
+		}
+		if (playerselect == 4) {
+			std::cin >> playerselect;
+		}
+		if (playerselect == 5) {
+			std::cin >> playerselect;
+		}
+		if (playerselect == 6) {
+			std::cin >> playerselect;
+		}
+		if (playerselect == 7) {
+			std::cin >> playerselect;
+		}
+		if (playerselect == 8) {
+			std::cin >> playerselect;
+		}
+		else
+		{
+			std::cout << "Incompatible input" << std::endl;
+		}
+	}*/
+
+	return 0;
+}
